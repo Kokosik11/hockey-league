04f9ab0d514a09a2a9de0181ab047c1346a23b52
Tim Demidov <demidovbest@gmail.com>
2021-04-06 20:15:47 +0300

    Добавил модель БД для команды и вывел в админке, будет дополняться

--- teams/admin.py
+++ teams/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Team
 
-# Register your models here.
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    list_display_links = ('slug', 'name',)
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ('name',)
+    ordering = ['name']
--- teams/models.py
+++ teams/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Team(models.Model):
+   name = models.CharField('Название команды', max_length=100)
+   year_of_foundation = models.PositiveSmallIntegerField('Год основания', blank=True)
+   league = models.CharField('Лига', max_length=50)
+   logo = models.ImageField('Лого команды', default='default.jpg', upload_to='team_images')
+   captain = models.CharField('Капитан', max_length=100)
+   coach = models.CharField('Тренер', max_length=100)
+   location = models.CharField('Местоположение', max_length=100)
+   slug = models.SlugField("Ссылка", max_length=130, unique=True, default='', help_text="ссылка на профиль команды")
+
+   def __str__(self):
+      return self.name
+
+   class Meta:
+      verbose_name = "Команда"
