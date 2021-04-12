e67938a2e8084e4e9739b5bcc67427afb61a8d20
Tim Demidov <demidovbest@gmail.com>
2021-04-13 00:00:00 +0300

    creating a match player statistics model. All models will be edited

--- match/admin.py
+++ match/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Match
+from .models import Match, PlayerMatchStatistics
 
 @admin.register(Match)
 class MatchAdmin(admin.ModelAdmin):
@@ -8,3 +8,9 @@ class MatchAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('first_team', 'second_team', 'date',)}
     search_fields = ('first_team', 'second_team', 'date')
     ordering = ['-date']
+
+@admin.register(PlayerMatchStatistics)
+class PlayerMatchStatisticsAdmin(admin.ModelAdmin):
+    list_display = ('player', 'match', "points", 'removes')
+    list_display_links = ('player', 'match',)
+    search_fields = ('player', 'match',)
--- match/models.py
+++ match/models.py
@@ -5,8 +5,8 @@ from players.models import Player
 
 class Match(models.Model):
   slug = models.SlugField("Ссылка", max_length=130, unique=True, default='', help_text="ссылка на матч")
-  first_team = models.ForeignKey(Team, related_name='first_team_matches', verbose_name="первая команда", on_delete=models.CASCADE, null=True, blank=True)
-  second_team = models.ForeignKey(Team, related_name='second_team_matches', verbose_name="вторая команда", on_delete=models.CASCADE, null=True, blank=True)
+  first_team = models.ForeignKey(Team, related_name='home_team_matches', verbose_name="первая команда", on_delete=models.CASCADE, null=True, blank=True)
+  second_team = models.ForeignKey(Team, related_name='away_team_matches', verbose_name="вторая команда", on_delete=models.CASCADE, null=True, blank=True)
   is_ended = models.BooleanField("Закончен", default=False)
   date = models.DateTimeField("Дата проведения")
   location = models.CharField("Место проведения", max_length=100)
@@ -17,3 +17,21 @@ class Match(models.Model):
   class Meta:
     verbose_name = "Матч"
     verbose_name_plural = "Матчи"
+
+class PlayerMatchStatistics(models.Model):
+  player = models.ForeignKey(Player, related_name="match_statistics", verbose_name="игрок", on_delete=models.CASCADE, null=True, blank=True)
+  match = models.ForeignKey(Match, related_name="statistics", verbose_name="матч", on_delete=models.CASCADE, null=True, blank=True)
+  goals = models.IntegerField("Голы", blank=True)
+  assists = models.IntegerField("Ассисты", blank=True)
+  removes = models.IntegerField("Удаления", blank=True)
+
+  def points(self):
+    points = self.goals + self.assists
+    return points
+
+  def __str__(self):
+    return self.player.surname
+
+  class Meta:
+    verbose_name = "Статистика"
+    verbose_name_plural = "Статистики"
