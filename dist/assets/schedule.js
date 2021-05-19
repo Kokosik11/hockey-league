ccbcaa597d73957ee13823fa8c618ac71e4bb3e7
Kokosik11 <lomjector98@gmail.com>
2021-05-19 21:44:00 +0300

    main page has finished

--- dist/assets/schedule.js
+++ dist/assets/schedule.js
@@ -6,91 +6,66 @@ class Schedule {
     months = ["январь", "февраль", "март", "апрель", "май", "июнь", "июль", "август", "сентябрь", "октябрь", "ноябрь", "декабрь"];
     date = new Date();
     month = this.date.getMonth();
+    days = [];
 
-    renderMonth = (value) => {
-        let monthDOM = document.querySelector('.month');
-        monthDOM.innerHTML = this.months[value];
+    setHeading = (DOMElement = this.DOMMonth) => {
+        let tmpMonth = new Date();
+        tmpMonth.setMonth(this.months.indexOf(this.months[this.month]));
+        DOMElement.innerHTML = `${this.months[this.month]}, ${this.date.getFullYear()}`;
+        console.log(this.date);
+        console.log(this.month);
     }
 
-    getPrevMonth = (date) => {
-        let prevMonthDays = [];
-        let _tmpDate = new Date(date);
-        _tmpDate.setMonth(date.getMonth() - 1);
-        _tmpDate.setDate(_tmpDate.daysInMonth());
+    renderMonth = () => {
+        this.getMonths();
 
-        _tmpDate.setDate(_tmpDate.daysInMonth() - _tmpDate.getDay() + 1);
-        
-        console.log(_tmpDate.daysInMonth() - _tmpDate.getDate());
-
-        let days = _tmpDate.getDate();
-        for(let i = 0; i < _tmpDate.daysInMonth() - days; i++) {
-            console.log(_tmpDate);
-
-            let _tmpMonth = { date: new Date(_tmpDate), content: []};
-            _tmpDate.setDate(_tmpDate.getDate() + 1);
-            prevMonthDays[i] = _tmpMonth;
+        const ceilDOM = document.querySelector(".ceils-content");
+        ceilDOM.innerHTML = "";
+        for(let i = 0; i < this.date.daysInMonth(); i++) {
+            let ceil = `<div class="date"><span class="day">${this.days[i].date.getDate()}</span><span class="date-content"></span></div>`
+            ceilDOM.innerHTML += ceil;
         }
 
-        if(prevMonthDays.length > 0 && prevMonthDays[prevMonthDays.length - 1].date.getDay() !== _tmpDate.daysInMonth() 
-            || prevMonthDays.length === 0); 
-        {
-            _tmpDate.setDate(_tmpDate.getDate());
-            let _tmpMonth = { date: new Date(_tmpDate), content: []};
-            prevMonthDays.push(_tmpMonth);
-        }
-        console.log(prevMonthDays);
     }
 
-    getNextMonth = (date) => {}
+    getMonths = () => {
+        this.days = [];
+        this.date.setDate(1);
 
-    getTableCeils = (date = this.date) => {
-        let _tmpDate = new Date();
-        _tmpDate.setMonth(date.getMonth());
-        _tmpDate.setFullYear(date.getFullYear())
-        _tmpDate.setDate(1);
-
-        let scheduleDates = [];
-
-        for(let i = 0; i < _tmpDate.daysInMonth(); i++) {
-            _tmpDate.setDate(i+1);
-            scheduleDates[i] = { date: new Date(_tmpDate), content: [] };
+        for(let i = 0; i < this.date.daysInMonth(); i++) {
+            let _tmpMonth = new Date(this.date);
+            _tmpMonth.setDate(i + 1);
+            this.days[i] = { date: _tmpMonth, content: [] };
         }
-        console.log(scheduleDates)
 
-        date.setDate(1);
-
-        if(date.getDay() !== 1) this.getPrevMonth(date);
+        // console.log(this.days);
     }
 
-    onArrowClick = () => {
-        const arrows = document.querySelectorAll('.arrow');
+    switchMonth = () => {
+        const arrows = document.querySelectorAll(".arrow");
+        this.renderMonth();
 
         arrows.forEach(arrow => {
-            arrow.addEventListener("click", e => {
-                let _tmpMonth = 0;
+            arrow.addEventListener("click", e => {                
+                if(+arrow.dataset.direction === 1 && this.month === 11) this.date.setFullYear(this.date.getFullYear() + 1);
+                if(+arrow.dataset.direction === -1 && this.month === 0) this.date.setFullYear(this.date.getFullYear() - 1);
 
-                if(arrow.dataset.direction === 'right') _tmpMonth++;
-                else if(arrow.dataset.direction === 'left') _tmpMonth--;
+                this.month += +arrow.dataset.direction;
 
-                this.month += _tmpMonth;
                 this.month = this.month === -1?11:this.month === 12?0:this.month;
-                this.renderMonth(this.month);
+                this.date.setMonth(this.month);
+                this.setHeading();
+                this.renderMonth();
 
-                let tmpDate = new Date();
-                tmpDate.setMonth(this.month);
-                this.getTableCeils(tmpDate);
             })
         })
     }
 
-    constructor() {
-        this.renderMonth(this.month);
-        this.onArrowClick();
-        let month = new Date();
-        month.setMonth(this.months.indexOf(this.months[3]));
-        this.getTableCeils(month);
+    constructor(DOMMonth) {
+        this.DOMMonth = document.querySelector(DOMMonth);
+        this.setHeading();
+        this.switchMonth();
     }
-
 }
 
-let schedule = new Schedule();
+let schedule = new Schedule(".month");
