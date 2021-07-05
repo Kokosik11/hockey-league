0cf25960a3312a260ba49ec87f44a7ec8c988b95
Tim <demidovbest@gmail.com>
2021-07-05 11:27:24 +0300

    added base layouts and styles of mainpage and teams templates

--- hockey/settings.py
+++ hockey/settings.py
@@ -131,6 +131,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
